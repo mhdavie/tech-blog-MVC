936724960b2b910056af95126719153d3ce9eb72
mdavie <marianahodniki1991@gmail.com>
2021-12-03 20:45:35 -0700

    added models to commnent post, user and finished associations on index.js

--- models/index.js
+++ models/index.js
@@ -6,13 +6,37 @@ const Post = require('./Comment');
 //hasMany Post and Comments
 //belongsTo Comments and User
 
+
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+})
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+})
+
+
 Post.belongsTo(User, {
     foreignkey: 'userId',
     onDelete: 'CASCADE'
 });
 
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+})
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+})
+
+
 module.exports = {
     User,
-    Comment,
     Post,
+    Comment
 };
